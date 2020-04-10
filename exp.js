22d5fe0d73815d1198b952259785ea6e73858288
Nathan <nathanhemesath@users.noreply.github.com>
2020-04-09 20:19:05 -0500

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,34 +1,30 @@
-
-	;(function(){
+;(function(){
 		
-		var init = function(){
-			// Inject the Div
-			setupFitWeird();
-		}
-		
-		var setupFitWeird = function(){
-
-			// Use jQuery to create a div with the text "Here is some text"
+		var setupTestMenu = function(){
+			
 			var div = $('<div></div>');
-			var newContent = '<ul class="tab-slider--tabs"><li class="tab-slider--trigger active exp-202-v0" rel="exp-202-v0">v0</li><li class="tab-slider--trigger exp-202-v1" rel="exp-202-v1">v1</li><li class="tab-slider--trigger exp-202-v2" rel="exp-202-v2">v2</li><li class="tab-slider--trigger exp-202-v3" rel="exp-202-v3">v3</li><li class="tab-slider--trigger exp-202-v4" rel="exp-202-v4">v4</li></ul>';
-		newContent += '<style>#tab-slider--nav{position:fixed;left:0;right:0;bottom:0;z-index:99999;width: 100%;}.tab-slider--tabs {display: block;float: left;margin: 0;padding: 0;list-style: none;position: relative;border-radius: 35px;overflow: hidden;background: #fff;height: 35px;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}.tab-slider--tabs .tab-slider--trigger.active:after {content: "";width: 100%;background: #345F90;height: 100%;position: absolute;top: 0;left: 0;right:0;-webkit-transition: all 250ms ease-in-out;transition: all 250ms ease-in-out;border-radius: 35px;z-index:-1;}.tab-slider--tabs.slide:after {left: 50%;}.tab-slider--trigger {width: 20%;font-size: 12px;line-height: 1;font-weight: bold;color: #345F90;text-transform: uppercase;text-align: center;padding: 11px 20px;z-index: 2;cursor: pointer;display: inline-block;-webkit-transition: color 250ms ease-in-out;transition: color 250ms ease-in-out;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}.tab-slider--trigger.active {color: #fff;position: relative;}</style>';
-
-
+			var newContent = '<ul class="tab-slider--tabs"><li class="tab-slider--trigger active" data-version="exp-202-v0">v0</li><li class="tab-slider--trigger" data-version="exp-202-v1">v1</li><li class="tab-slider--trigger" data-version="exp-202-v2">v2</li><li class="tab-slider--trigger" data-version="exp-202-v3">v3</li><li class="tab-slider--trigger" data-version="exp-202-v4">v4</li></ul>';
+			newContent += '<style>#tab-slider--nav{position:fixed;left:0;right:0;bottom:0;z-index:99999;width: 100%;}.tab-slider--tabs {display: block;float: left;margin: 0;padding: 0;list-style: none;position: relative;border-radius: 35px;overflow: hidden;background: #fff;height: 35px;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}.tab-slider--trigger {width: 20%;font-size: 12px;line-height: 1;font-weight: bold;color: #345F90;text-transform: uppercase;text-align: center;padding: 11px 20px;z-index: 2;cursor: pointer;display: inline-block;-webkit-transition: color 250ms ease-in-out;transition: color 250ms ease-in-out;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;}.tab-slider--trigger.active {color: #fff;position: relative;}.tab-slider--trigger.active:after {content: "";width: 100%;background: #345F90;height: 100%;width:100%;position: absolute;top: 0;left: 0;right:0;-webkit-transition: all 250ms ease-in-out;transition: all 250ms ease-in-out;border-radius: 35px;z-index:-1;}</style>';
+			
 			div.html(newContent);
 			div.attr('id', 'tab-slider--nav');
-
-			$('#tab-slider--nav li').click(function(){
-				$(".active").removeClass("active");
-				$(this).addClass("active");				
-				$('body').removeClass("exp-202-v0 exp-202-v1 exp-202-v2 exp-202-v3 exp-202-v4").addClass("" + $(".active").attr("class").match(/exp-202-v[0-9]/));
-			});
-			$('body').removeClass("exp-202-v0 exp-202-v1 exp-202-v2 exp-202-v3 exp-202-v4").addClass("" + $(".active").attr("class").match(/exp-202-v[0-9]/));
-			
-			// Append the div to the <body> tag
 			$('body').append(div);
-
 		};
 		
+		var setupTestAction = function(){
+			$('#tab-slider--nav li').click(function(){
+				$(".tab-slider--trigger").removeClass("active");
+				$(this).addClass("active");
+				var expClass = $(this).attr('data-version');
+				$('body').removeClass("exp-202-v0 exp-202-v1 exp-202-v2 exp-202-v3 exp-202-v4").addClass("" + expClass);
+			});
+		};
+		
+		var init = function(){
+			setupTestMenu();
+			setupTestAction();
+		}
+		
 		init();
-
+		
 	})(window);	
